ffa1da9de59c00916a97b1b120d5c4d88059ca4b
patkoton <patkoton@yahoo.com>
2023-03-22 13:00:04 +0100

    Initial API request with Axios and TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+// Defining a type
+export type Crypto = {
+  ath: number
+  atl: number
+  current_price: number
+  id: string
+  name: string
+  symbol: string
+  high_24h: number
+  low_24h: number
+}
+
 function App() {
+  const [cryptos, setCryptos] = useState<Crypto[] | null>();
+  useEffect(() => {
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+      axios.get(url).then((response) => {
+        setCryptos(response.data);
+      })
+  }, []) // use empty dependency to execute on a page load
+  
   return (
     <div className="App">
-     <h1>Hello</h1>
+     { cryptos ? cryptos.map((crypto) => {
+      return <p>{crypto.name + ' $' + crypto.current_price}</p>
+     }) : null }
     </div>
   );
 }
